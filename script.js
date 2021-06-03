877c6966067f847d11f627f2f01fddf6c8076552
gustave ploix <vilgotploix@gmail.com>
2021-06-03 17:21:59 +0200

    ajout d'une fonctionnalité

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 var rellax = new Rellax('.rellax');
+
+const pres = document.getElementsByClassName('nomPres')[0];
+const page = document.getElementsByClassName('page')[0];
+
+
+setTimeout(() => {
+
+    pres.classList.add('invisible')
+
+    setTimeout(() => {
+        page.classList.remove('invisible')
+    }, 1000)
+
+}, 1500)
