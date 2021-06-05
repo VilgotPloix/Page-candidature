676868a122b593891ff3e46e84fbf3d169bb59ac
gustave ploix <vilgotploix@gmail.com>
2021-06-05 22:00:27 +0200

    ajout du clignottement

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ var rellax = new Rellax('.rellax');
 
 const pres = document.getElementsByClassName('nomPres')[0];
 const page = document.getElementsByClassName('page')[0];
+const body = document.querySelector('body')
 
+const audio = new Audio('./media/narkosis.mp3');
+
+console.log(audio);
 
 setTimeout(() => {
 
@@ -10,6 +14,13 @@ setTimeout(() => {
 
     setTimeout(() => {
         page.classList.remove('invisible')
+        audio.play()
     }, 1000)
 
 }, 1500)
+
+audio.addEventListener('play', () => {
+    setInterval(() => {
+        body.classList.toggle('bg')
+    }, 500);
+})
